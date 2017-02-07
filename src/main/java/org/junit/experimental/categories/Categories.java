a482e0cd851d8bfcbb053df74fb02e37c4ea0fa4
JavaSking <597200818@qq.com>
2017-02-07 09:05:19 +0800

    注释

--- src/main/java/org/junit/experimental/categories/Categories.java
+++ src/main/java/org/junit/experimental/categories/Categories.java
@@ -58,148 +58,270 @@ import org.junit.runners.model.RunnerBuilder;
  * }
  * </pre>
  */
+
+/**
+ * 
+ * @author JavaSking 2017年2月6日
+ */
 public class Categories extends Suite {
-    // the way filters are implemented makes this unnecessarily complicated,
-    // buggy, and difficult to specify.  A new way of handling filters could
-    // someday enable a better new implementation.
-    // https://github.com/KentBeck/junit/issues/issue/172
 
-    @Retention(RetentionPolicy.RUNTIME)
-    public @interface IncludeCategory {
-        public Class<?> value();
-    }
+	/**
+	 * IncludeCategory注解用于标注包括的组。
+	 * 
+	 * @author 注释By JavaSking 2017年2月6日
+	 */
+	@Retention(RetentionPolicy.RUNTIME)
+	public @interface IncludeCategory {
 
-    @Retention(RetentionPolicy.RUNTIME)
-    public @interface ExcludeCategory {
-        public Class<?> value();
-    }
+		/**
+		 * 包括的组。
+		 * 
+		 * @return 包括的组。
+		 */
+		public Class<?> value();
+	}
 
-    public static class CategoryFilter extends Filter {
-        public static CategoryFilter include(Class<?> categoryType) {
-            return new CategoryFilter(categoryType, null);
-        }
+	/**
+	 * ExcludeCategory注解用于标注排除的组。
+	 * 
+	 * @author 注释By JavaSking 2017年2月6日
+	 */
+	@Retention(RetentionPolicy.RUNTIME)
+	public @interface ExcludeCategory {
 
-        private final Class<?> fIncluded;
+		/**
+		 * 排除的组。
+		 * 
+		 * @return 排除的组。
+		 */
+		public Class<?> value();
+	}
 
-        private final Class<?> fExcluded;
+	/**
+	 * 组过滤器。
+	 * 
+	 * @author 注释By JavaSking 2017年2月6日
+	 */
+	public static class CategoryFilter extends Filter {
 
-        public CategoryFilter(Class<?> includedCategory,
-                Class<?> excludedCategory) {
-            fIncluded = includedCategory;
-            fExcluded = excludedCategory;
-        }
+		/**
+		 * 包括目标组。
+		 * 
+		 * @param categoryType
+		 *          目标组。
+		 * @return 当前组过滤器。
+		 */
+		public static CategoryFilter include(Class<?> categoryType) {
 
-        @Override
-        public String describe() {
-            return "category " + fIncluded;
-        }
+			return new CategoryFilter(categoryType, null);
+		}
 
-        @Override
-        public boolean shouldRun(Description description) {
-            if (hasCorrectCategoryAnnotation(description)) {
-                return true;
-            }
-            for (Description each : description.getChildren()) {
-                if (shouldRun(each)) {
-                    return true;
-                }
-            }
-            return false;
-        }
+		/**
+		 * 包括的组。
+		 */
+		private final Class<?> fIncluded;
 
-        private boolean hasCorrectCategoryAnnotation(Description description) {
-            List<Class<?>> categories = categories(description);
-            if (categories.isEmpty()) {
-                return fIncluded == null;
-            }
-            for (Class<?> each : categories) {
-                if (fExcluded != null && fExcluded.isAssignableFrom(each)) {
-                    return false;
-                }
-            }
-            for (Class<?> each : categories) {
-                if (fIncluded == null || fIncluded.isAssignableFrom(each)) {
-                    return true;
-                }
-            }
-            return false;
-        }
+		/**
+		 * 排除的组。
+		 */
+		private final Class<?> fExcluded;
 
-        private List<Class<?>> categories(Description description) {
-            ArrayList<Class<?>> categories = new ArrayList<Class<?>>();
-            categories.addAll(Arrays.asList(directCategories(description)));
-            categories.addAll(Arrays.asList(directCategories(parentDescription(description))));
-            return categories;
-        }
+		/**
+		 * 构造一个组过滤器。
+		 * 
+		 * @param includedCategory
+		 *          包括的组。
+		 * @param excludedCategory
+		 *          排除的组。
+		 */
+		public CategoryFilter(Class<?> includedCategory, Class<?> excludedCategory) {
 
-        private Description parentDescription(Description description) {
-            Class<?> testClass = description.getTestClass();
-            if (testClass == null) {
-                return null;
-            }
-            return Description.createSuiteDescription(testClass);
-        }
+			fIncluded = includedCategory;
+			fExcluded = excludedCategory;
+		}
 
-        private Class<?>[] directCategories(Description description) {
-            if (description == null) {
-                return new Class<?>[0];
-            }
-            Category annotation = description.getAnnotation(Category.class);
-            if (annotation == null) {
-                return new Class<?>[0];
-            }
-            return annotation.value();
-        }
-    }
+		@Override
+		public String describe() {
 
-    public Categories(Class<?> klass, RunnerBuilder builder)
-            throws InitializationError {
-        super(klass, builder);
-        try {
-            filter(new CategoryFilter(getIncludedCategory(klass),
-                    getExcludedCategory(klass)));
-        } catch (NoTestsRemainException e) {
-            throw new InitializationError(e);
-        }
-        assertNoCategorizedDescendentsOfUncategorizeableParents(getDescription());
-    }
+			return "category " + fIncluded;
+		}
 
-    private Class<?> getIncludedCategory(Class<?> klass) {
-        IncludeCategory annotation = klass.getAnnotation(IncludeCategory.class);
-        return annotation == null ? null : annotation.value();
-    }
+		@Override
+		public boolean shouldRun(Description description) {
 
-    private Class<?> getExcludedCategory(Class<?> klass) {
-        ExcludeCategory annotation = klass.getAnnotation(ExcludeCategory.class);
-        return annotation == null ? null : annotation.value();
-    }
+			if (hasCorrectCategoryAnnotation(description)) {
+				return true;
+			}
+			for (Description each : description.getChildren()) {
+				if (shouldRun(each)) {
+					return true;
+				}
+			}
+			return false;
+		}
 
-    private void assertNoCategorizedDescendentsOfUncategorizeableParents(Description description) throws InitializationError {
-        if (!canHaveCategorizedChildren(description)) {
-            assertNoDescendantsHaveCategoryAnnotations(description);
-        }
-        for (Description each : description.getChildren()) {
-            assertNoCategorizedDescendentsOfUncategorizeableParents(each);
-        }
-    }
+		/**
+		 * 判断目标测试元素是否要被测试。<br>
+		 * 需要测试的情况有：
+		 * <ul>
+		 * <li>1、未被分组且未指定IncludeCategory。
+		 * <li>2、被分组，且被IncludeCategory包括。
+		 * <li>3、被分组，且未指定IncludeCategory。
+		 * <ul>
+		 * @param description 目标测试元素的测试内容描述信息。
+		 * @return 如果目标测试元素要被测试则返回true，否则返回false。
+		 */
+		private boolean hasCorrectCategoryAnnotation(Description description) {
 
-    private void assertNoDescendantsHaveCategoryAnnotations(Description description) throws InitializationError {
-        for (Description each : description.getChildren()) {
-            if (each.getAnnotation(Category.class) != null) {
-                throw new InitializationError("Category annotations on Parameterized classes are not supported on individual methods.");
-            }
-            assertNoDescendantsHaveCategoryAnnotations(each);
-        }
-    }
+			List<Class<?>> categories = categories(description);
+			/* 1、未被分组且未指定IncludeCategory则需要测试 */
+			if (categories.isEmpty()) {
+				return fIncluded == null;
+			}
+			
+			for (Class<?> each : categories) {
+				/* 2、被分组且被IncludeCategory包括，则不需要测试 */
+				if (fExcluded != null && fExcluded.isAssignableFrom(each)) {
+					return false;
+				}
+			}
+			for (Class<?> each : categories) {
+				/* 2、被分组且（被指定IncludeCategory包括或未指定IncludeCategory），则需要测试 */
+				if (fIncluded == null || fIncluded.isAssignableFrom(each)) {
+					return true;
+				}
+			}
+			/* 2、其他情况不需要测试 */
+			return false;
+		}
 
-    // If children have names like [0], our current magical category code can't determine their
-    // parentage.
-    private static boolean canHaveCategorizedChildren(Description description) {
-        for (Description each : description.getChildren()) {
-            if (each.getTestClass() == null) {
-                return false;
-            }
-        }
-        return true;
-    }
+		/**
+		 * 获取测试元素（测试方法或测试类）所属组。
+		 * 
+		 * @param description
+		 *          测试元素的测试内容描述信息。
+		 * @return 目标测试元素所属组。
+		 */
+		private List<Class<?>> categories(Description description) {
+
+			ArrayList<Class<?>> categories = new ArrayList<Class<?>>();
+			categories.addAll(Arrays.asList(directCategories(description)));
+			categories.addAll(Arrays.asList(directCategories(parentDescription(description))));
+			return categories;
+		}
+
+		/**
+		 * 获取父测试元素的测试内容描述信息（到测试类为值，对于测试类的组合将直接返回null）。
+		 * 
+		 * @param description
+		 *          测试元素的测试内容描述信息。
+		 * @return 父测试元素的测试内容描述信息。
+		 */
+		private Description parentDescription(Description description) {
+
+			Class<?> testClass = description.getTestClass();
+			if (testClass == null) {
+				return null;
+			}
+			return Description.createSuiteDescription(testClass);
+		}
+
+		/**
+		 * 获取测试元素（测试方法或测试类）所属组，没有则返回空组。
+		 * 
+		 * @param description
+		 *          测试元素的测试内容描述信息，可能为null。
+		 * @return 目标测试元素所属组或空组。
+		 */
+		private Class<?>[] directCategories(Description description) {
+
+			if (description == null) {
+				return new Class<?>[0];
+			}
+			Category annotation = description.getAnnotation(Category.class);
+			if (annotation == null) {
+				return new Class<?>[0];
+			}
+			return annotation.value();
+		}
+	}
+
+	/**
+	 * 构造一个Categories运行器。
+	 * 
+	 * @param klass 测试类。
+	 * @param builder 运行器创建工厂。
+	 * @throws InitializationError 运行器初始化异常。
+	 */
+	public Categories(Class<?> klass, RunnerBuilder builder) throws InitializationError {
+		super(klass, builder);
+		try {
+			filter(new CategoryFilter(getIncludedCategory(klass), getExcludedCategory(klass)));
+		} catch (NoTestsRemainException e) {
+			throw new InitializationError(e);
+		}
+		assertNoCategorizedDescendentsOfUncategorizeableParents(getDescription());
+	}
+
+	/**
+	 * 获取目标测试类的IncludedCategory注解属性值，没有则返回null。
+	 * @param klass 测试类。
+	 * @return 目标测试类的IncludedCategory注解属性值，没有则返回null。
+	 */
+	private Class<?> getIncludedCategory(Class<?> klass) {
+
+		IncludeCategory annotation = klass.getAnnotation(IncludeCategory.class);
+		return annotation == null ? null : annotation.value();
+	}
+
+	/**
+	 * 获取目标测试类的ExcludeCategory注解属性值，没有则返回null。
+	 * @param klass 测试类。
+	 * @return 目标测试类的ExcludeCategory注解属性值，没有则返回null。
+	 */
+	private Class<?> getExcludedCategory(Class<?> klass) {
+
+		ExcludeCategory annotation = klass.getAnnotation(ExcludeCategory.class);
+		return annotation == null ? null : annotation.value();
+	}
+
+	/**
+	 * 
+	 * @param description 测试内容描述信息。
+	 * @throws InitializationError 运行器初始化异常。
+	 */
+	private void assertNoCategorizedDescendentsOfUncategorizeableParents(Description description) throws InitializationError {
+
+		if (!canHaveCategorizedChildren(description)) {
+			assertNoDescendantsHaveCategoryAnnotations(description);
+		}
+		for (Description each : description.getChildren()) {
+			assertNoCategorizedDescendentsOfUncategorizeableParents(each);
+		}
+	}
+
+	private void assertNoDescendantsHaveCategoryAnnotations(Description description) throws InitializationError {
+
+		for (Description each : description.getChildren()) {
+			if (each.getAnnotation(Category.class) != null) {
+				throw new InitializationError("Category annotations on Parameterized classes are not supported on individual methods.");
+			}
+			assertNoDescendantsHaveCategoryAnnotations(each);
+		}
+	}
+
+	/**
+	 * 
+	 * @param description
+	 * @return
+	 */
+	private static boolean canHaveCategorizedChildren(Description description) {
+
+		for (Description each : description.getChildren()) {
+			if (each.getTestClass() == null) {
+				return false;
+			}
+		}
+		return true;
+	}
 }
--- src/main/java/org/junit/experimental/categories/Category.java
+++ src/main/java/org/junit/experimental/categories/Category.java
@@ -4,40 +4,57 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
 /**
- * Marks a test class or test method as belonging to one or more categories of tests.
- * The value is an array of arbitrary classes.
- *
- * This annotation is only interpreted by the Categories runner (at present).
- *
- * For example:
+ * Category注解，用于对测试用例分组。<br>
+ * 测试方法或测试类可以同属多个分组。类上的Category注解不会把方法上的Category注解覆盖。<br>
+ * 
+ * 例子：
+ * 
  * <pre>
- * public interface FastTests {}
- * public interface SlowTests {}
- *
- * public static class A {
+ * public interface AttributeFun{ 
+ * 
+ * }
+ * public interface BehaviorFun{ 
+ * 
+ * }
+ * &#064;Category(AttributeFun.class)
  * &#064;Test
- * public void a() {
- * fail();
- * }
- *
- * &#064;Category(SlowTests.class)
+ * public void testGetAge()｛
+ * 	int age = person.getAge();
+ * 	assertEquals(3, age);
+ * ｝
+ * 
+ * &#064;Category(AttributeFun.class)
  * &#064;Test
- * public void b() {
- * }
- * }
- *
- * &#064;Category({SlowTests.class, FastTests.class})
- * public static class B {
+ * public void testGetName()｛
+ * 	String name = person.getName();
+ * 	assertEquals("Willard", name);
+ * ｝
+ * 
+ * &#064;Category(BehaviorFun.class)
  * &#064;Test
- * public void c() {
- *
- * }
- * }
+ * public void testTalk()｛
+ * 	String message = person.talkTo("Jimy");
+ * 	assertNotNull(message);
+ * ｝
+ * 
+ * &#064;Category(BehaviorFun.class)
+ * &#064;Test(timeout=200)
+ * public void testWalk()｛
+ * 	person.walk();
+ * ｝
+ * 
+ * 上述将测试用例分为两组：属性方法（GetAge，GetName）和行为方法（Talk，Walk）的测试用例。
  * </pre>
- *
- * For more usage, see code example on {@link Categories}.
+ * 
+ * @author 注释By JavaSking 2017年2月6日
  */
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Category {
-    Class<?>[] value();
+
+	/**
+	 * 测试用例所属组。
+	 * 
+	 * @return 测试用例所属组。
+	 */
+	Class<?>[] value();
 }
